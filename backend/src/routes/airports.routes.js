11f294e35e186b4cd94da77a57d81819fb08679b
Talha Hanif <talhahanif1999@gmail.com>
2024-05-27 02:27:12 +0500

    new empty branch created named v1

--- backend/src/routes/airports.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAirportByName } from "../controllers/airports.controller.js";
-
-const airportsRouter = express.Router();
-
-// Get airport
-airportsRouter.get("/", getAirportByName);
-
-export { airportsRouter };
--- frontend/src/components/List.jsx
+++
@@ -1,9 +0,0 @@
-const List = ({ children }) => {
-  return (
-    <p className="text-base text-body-color dark:text-dark-6 m-[10px]">
-      ✔ {children}
-    </p>
-  );
-};
-
-export default List;
