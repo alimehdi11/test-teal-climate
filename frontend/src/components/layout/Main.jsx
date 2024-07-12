e69fadb697ec059eadb7df1e2cec04f5179c0b5c
Abdullah <abdullahsoomro.official@gmail.com>
2024-07-12 13:39:05 +0500

    sidebar content height fixed

--- frontend/src/components/layout/Main.jsx
+++ frontend/src/components/layout/Main.jsx
@@ -1,8 +1,6 @@
 const Main = ({ children }) => {
   return (
-    <div className="mt-[66px] md:mt-0 w-[100vw] h-full md:w-[75vw]">
-      {children}
-    </div>
+    <div className="mt-[66px] md:mt-0 w-[100vw] md:w-[75vw]">{children}</div>
   );
 };
 
