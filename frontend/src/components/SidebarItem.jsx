9ddea836806be989bf28c02d9e91691b95aa6da6
Abdullah <abdullahsoomro.official@gmail.com>
2024-08-31 16:37:34 +0500

    sidebar icons updated

--- frontend/src/components/SidebarItem.jsx
+++ frontend/src/components/SidebarItem.jsx
@@ -2,7 +2,7 @@ const SidebarItem = ({ children, className, ...props }) => {
   return (
     <div
       className={(
-        "text-tc-black font-medium flex items-center gap-x-3 py-3 px-4 rounded-md hover:bg-tc-indigo-light hover:text-tc-blue [&_svg:first-child]:size-5 [&_svg:first-child]:min-h-5 [&_svg:first-child]:min-w-5 " +
+        "text-tc-black font-medium flex items-center gap-x-3 py-3 px-4 rounded-md hover:bg-tc-indigo-light hover:text-tc-blue [&_img:first-child]:size-5 [&_img:first-child]:min-h-5 [&_img:first-child]:min-w-5 " +
         className
       ).trim()}
       {...props}
