dff6f849ee37873058e64efd8368cb4ede0d894a
Abdullah <abdullahsoomro.official@gmail.com>
2024-10-10 23:43:03 +0500

    updateActivityById updated to match new payload shape

--- backend/src/services/businessUnitsActivites.services.js
+++ backend/src/services/businessUnitsActivites.services.js
@@ -5,9 +5,8 @@ import { BusinessUnit } from "../models/businessUnit.model.js";
 import { Reit } from "../models/reit.model.js";
 import { Activity } from "../models/activity.model.js";
 import { ElectricVehicle } from "../models/electricVehicle.model.js";
-import { json } from "sequelize";
 
-const calculateGHGEmissions = (records, payload) => {
+const calculateActivityGHGEmissions = (records, payload) => {
   let CO2e = 0;
   let CO2e_of_CO2 = 0;
   let CO2e_of_CH4 = 0;
@@ -91,7 +90,7 @@ const createActivity = async (req, res) => {
           level2,
           level3,
           level4,
-          // level5 ==> excluding deliberately
+          // level5, ==> excluding deliberately
           unitOfMeasurement,
         },
       });
@@ -123,8 +122,8 @@ const createActivity = async (req, res) => {
         where: {
           scope,
           level1,
-          // level2,
-          // level3,
+          // level2, ==> excluding deliberately
+          // level3, ==> excluding deliberately
           level4,
           level5,
           unitOfMeasurement,
@@ -135,7 +134,7 @@ const createActivity = async (req, res) => {
         // distanceInKm = distanceInMiles * 1.60934
         payload.quantity = Number((payload.quantity * 1.60934).toFixed(4));
       }
-      payload = calculateGHGEmissions(activityRecords, payload);
+      payload = calculateActivityGHGEmissions(activityRecords, payload);
     } else if (
       [
         "Electricity",
@@ -160,7 +159,7 @@ const createActivity = async (req, res) => {
           level2,
           level3: country,
           level4: region,
-          // level5,
+          // level5, ==> excluding deliberately
           unitOfMeasurement,
         },
       });
@@ -214,48 +213,171 @@ const updateActivityById = async (req, res) => {
   try {
     const { id } = req.params;
     const {
-      unitOfMeasurement,
-      businessUnitId,
-      quantity,
       scope,
-      level1Category,
       level1,
+      month,
+      businessUnitId,
+      level1Category,
       level2,
       level3,
       level4,
       level5,
-      CO2e,
-      CO2e_of_CO2,
-      CO2e_of_CH4,
-      CO2e_of_N2O,
-      month,
-      // year,
+      unitOfMeasurement,
+      quantity,
+      marketBasedEmissionFactor, // This will only availabe for "Scope 2"-"marketBased"
     } = req.body;
-    await BusinessUnitActivity.update(
-      {
-        unitOfMeasurement,
-        businessUnitId,
-        quantity,
-        scope,
-        level1Category,
-        level1,
-        level2,
-        level3,
-        level4,
-        level5,
-        CO2e,
-        CO2e_of_CO2,
-        CO2e_of_CH4,
-        CO2e_of_N2O,
-        month,
-        // year,
-      },
-      {
+    let payload = {
+      userId: req.user.id,
+      scope,
+      level1,
+      month,
+      businessUnitId,
+      level1Category,
+      level2,
+      level3,
+      level4,
+      level5,
+      unitOfMeasurement,
+      quantity,
+      marketBasedEmissionFactor,
+    };
+    if (scope === "Scope 2" && level5 === "marketBased") {
+      const CO2e = payload.quantity * payload.marketBasedEmissionFactor;
+      delete payload.marketBasedEmissionFactor;
+      await BusinessUnitActivity.create({ ...payload, CO2e });
+      return res.status(200).json({ message: "Activity created sucessfully" });
+    } else if (
+      scope === "Scope 2" &&
+      (level1 === "Passenger Evs" || level1 === "Delivery Evs")
+    ) {
+      /**
+       *  Formula:
+       *  Distance travelled in km/miles (quantity)
+       *  x Electricity consumption per km/miles (fetch from electricVehicle table)
+       *  x Electricity emission factor of the country & region (fetch country/region from businessUnits table based on businessUnitId after that filter emission factors from activities based on Electricity and country/region)
+       */
+      const electricVehicleRecord = await ElectricVehicle.findOne({
         where: {
-          id,
+          scope,
+          level1,
+          level2,
+          level3,
+          level4,
+          // level5, ==> excluding deliberately
+          unitOfMeasurement,
         },
+      });
+      payload.quantity =
+        payload.quantity * electricVehicleRecord.electricityConsumptionPerUnit;
+      const businessUnitRecord = await BusinessUnit.findOne({
+        where: { id: businessUnitId },
+      });
+      const { country, region } = businessUnitRecord;
+      const activityRecords = await Activity.findAll({
+        where: {
+          scope,
+          level1: "Electricity",
+          level2: "Electricity generated",
+          level3: country,
+          level4: region,
+          level5,
+          unitOfMeasurement,
+        },
+      });
+      payload = calculateGHGEmissions(activityRecords, payload);
+    } else if (
+      scope === "Scope 3" &&
+      (level1 === "Business travel- air" ||
+        level1 === "WTT- business travel- air")
+    ) {
+      // Deal with airports data
+      const activityRecords = await Activity.findAll({
+        where: {
+          scope,
+          level1,
+          // level2, ==> excluding deliberately
+          // level3, ==> excluding deliberately
+          level4,
+          level5,
+          unitOfMeasurement,
+        },
+      });
+      // convert distance to "km" if "unitOfMeasurement" is "passenger.km"
+      if (payload.unitOfMeasurement === "passenger.km") {
+        // distanceInKm = distanceInMiles * 1.60934
+        payload.quantity = Number((payload.quantity * 1.60934).toFixed(4));
       }
-    );
+      payload = calculateActivityGHGEmissions(activityRecords, payload);
+    } else if (
+      [
+        "Electricity",
+        "Electricity TandD",
+        "WTT- electricity (generation)",
+        "WTT- electricity (TandD)",
+        "WTT- electricity",
+        "Managed assets- electricity",
+        "WTT- electricity (T&D)",
+        "Electricity T&D",
+      ].includes(payload.level1)
+    ) {
+      // Handle special level1
+      const businessUnitRecord = await BusinessUnit.findOne({
+        where: { id: businessUnitId },
+      });
+      const { country, region } = businessUnitRecord;
+      const activityRecords = await Activity.findAll({
+        where: {
+          scope,
+          level1,
+          level2,
+          level3: country,
+          level4: region,
+          // level5, ==> excluding deliberately
+          unitOfMeasurement,
+        },
+      });
+      if (activityRecords.length === 0) {
+        return res.status(200).json({
+          message:
+            "No electricity emission factor found for the country/region",
+        });
+      } else if (activityRecords.length > 0) {
+        let emissionFactorAvailable = true;
+        activityRecords.forEach((record) => {
+          if (
+            record.greenHouseGas === "kg CO2e" &&
+            record.greenHouseGasEmissionFactor === 0
+          ) {
+            emissionFactorAvailable = false;
+          }
+        });
+        if (!emissionFactorAvailable) {
+          return res.status(200).json({
+            message:
+              "No electricity emission factor found for the country/region",
+          });
+        }
+      }
+      payload = calculateGHGEmissions(activityRecords, payload);
+    } else {
+      const activityRecords = await Activity.findAll({
+        where: {
+          scope,
+          level1,
+          level2,
+          level3,
+          level4,
+          level5,
+          unitOfMeasurement,
+        },
+      });
+      payload = calculateGHGEmissions(activityRecords, payload);
+    }
+    await BusinessUnitActivity.update(payload, {
+      where: {
+        id,
+      },
+    });
     return res.status(200).json({ message: "Activity created sucessfully" });
   } catch (error) {
     console.log("Could not updateBusinessUnitActivity -> updateActivityById");
